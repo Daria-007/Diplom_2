ae340a459a4ff3c7e2bbe35e4c6eed0cba01f007
Daria Ermolaeva <ermolaeva.daria007@yandex.ru>
2024-05-01 12:50:37 +0300

    add login tests

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -1,12 +1,21 @@
+import io.qameta.allure.Step;
+import io.restassured.response.ValidatableResponse;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
 
-public class UserLoginTest  extends BaseTest{
+public class UserLoginTest extends BaseTest {
     private BurgerServiceUserImpl burgerServiceUser;
     private User testUser;
+    private String accessToken;
+
     @Before
     public void setUp() {
         burgerServiceUser = new BurgerServiceUserImpl(REQUEST_SPECIFICATION, RESPONSE_SPECIFICATION);
+        testUser = User.create("test-data@yandex.ru", "password", "Username");
+        ValidatableResponse response = burgerServiceUser.createUser(testUser);
+        accessToken = response.extract().path("accessToken");
     }
 
     @After
@@ -16,5 +25,22 @@ public class UserLoginTest  extends BaseTest{
         }
     }
 
+    @Test
+    @Step("Logging in with valid credentials")
+    public void testLoginWithValidCredentials() {
+        Credentials validCredentials = Credentials.fromUser(testUser);
+        burgerServiceUser.login(validCredentials)
+                .statusCode(200)
+                .body("success", equalTo(true));
+    }
 
+    @Test
+    @Step("Logging in with invalid credentials")
+    public void testLoginWithInvalidCredentials() {
+        Credentials invalidCredentials = new Credentials("nonexistent@example.com", "wrongPassword");
+        burgerServiceUser.login(invalidCredentials)
+                .statusCode(401)
+                .body("success", equalTo(false))
+                .body("message", equalTo("email or password are incorrect"));
+    }
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,4 @@
 import io.qameta.allure.Step;
-import io.restassured.response.ValidatableResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +8,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class UserTest extends BaseTest {
     private BurgerServiceUserImpl burgerServiceUser;
     private User testUser;
+
     @Before
     public void setUp() {
         burgerServiceUser = new BurgerServiceUserImpl(REQUEST_SPECIFICATION, RESPONSE_SPECIFICATION);
@@ -24,7 +24,7 @@ public class UserTest extends BaseTest {
     @Test
     @Step("Creating a unique user")
     public void testCreateUniqueUser() {
-        User testUser = User.create("test-data@yandex.ru", "password", "Username");
+        testUser = User.create("test-data@yandex.ru", "password", "Username");
         burgerServiceUser.createUser(testUser)
                 .statusCode(403)
                 .body("success", equalTo(false))
@@ -35,8 +35,8 @@ public class UserTest extends BaseTest {
     @Step("Creating a duplicate user")
     public void testCreateDuplicateUser() {
         // First, let's create a user
-        User existingUser = User.create("test-data@yandex.ru", "password", "Username");
-        burgerServiceUser.createUser(existingUser)
+        testUser = User.create("test-data@yandex.ru", "password", "Username");
+        burgerServiceUser.createUser(testUser)
                 .statusCode(403)
                 .body("success", equalTo(false))
                 .body("message", equalTo("User already exists"));
@@ -45,8 +45,8 @@ public class UserTest extends BaseTest {
     @Test
     @Step("Creating a user with missing field")
     public void testCreateUserWithMissingField() {
-        User incompleteUser = User.create("test-data@yandex.ru", "password", null); // Missing name
-        burgerServiceUser.createUser(incompleteUser)
+        testUser = User.create("test-data@yandex.ru", "password", null); // Missing name
+        burgerServiceUser.createUser(testUser)
                 .statusCode(403)
                 .body("success", equalTo(false))
                 .body("message", equalTo("Email, password and name are required fields"));
